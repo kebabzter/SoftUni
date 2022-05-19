36820b82ee720831ee899afdbbe1924a6f3d465e
kebabzter <gabzterthesecond@gmail.com>
2022-05-19 20:10:12 +0300

    Syntax Functions and Statements exercise

--- JS Advanced/JSSyntaxFunctionsAndStatements/validity-checker.js
+++ JS Advanced/JSSyntaxFunctionsAndStatements/validity-checker.js
@@ -5,7 +5,8 @@ function validityChecker(x1, y1, x2, y2) {
     y2 = Number(y2);
 
     let formula = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
-    console.log(formula);
+
+    //TODO: Finish the task
 }
 
 validityChecker(3, 0, 0, 4);
